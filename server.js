835dd43ff682c011986f0dae3233eff21f595beb
Abobyz83937e89464 <sinelniknikita41@gmail.com>
2026-02-25 00:34:40 +0200

    Refactor camera initialization and video processing

--- server.js
+++ server.js
@@ -2,50 +2,58 @@ const video = document.getElementById("video");
 const canvas = document.getElementById("canvas");
 const ctx = canvas.getContext("2d");
 
-navigator.mediaDevices.getUserMedia({ video: true })
-  .then(stream => {
-    video.srcObject = stream;
+async function startCamera() {
+  const stream = await navigator.mediaDevices.getUserMedia({
+    video: { facingMode: "user" }
   });
+  video.srcObject = stream;
 
-video.addEventListener("loadedmetadata", () => {
-  canvas.width = video.videoWidth;
-  canvas.height = video.videoHeight;
-});
+  video.onloadedmetadata = () => {
+    canvas.width = video.videoWidth;
+    canvas.height = video.videoHeight;
+    waitForOpenCV();
+  };
+}
+
+function waitForOpenCV() {
+  if (typeof cv !== "undefined" && cv.Mat) {
+    processVideo();
+  } else {
+    setTimeout(waitForOpenCV, 100);
+  }
+}
 
 function processVideo() {
-  if (typeof cv === "undefined") {
-    requestAnimationFrame(processVideo);
-    return;
-  }
+  ctx.drawImage(video, 0, 0, canvas.width, canvas.height);
 
-  let src = new cv.Mat(canvas.height, canvas.width, cv.CV_8UC4);
+  let src = cv.imread(canvas);
   let hsv = new cv.Mat();
   let mask = new cv.Mat();
-
-  ctx.drawImage(video, 0, 0, canvas.width, canvas.height);
-  let imageData = ctx.getImageData(0, 0, canvas.width, canvas.height);
-  src.data.set(imageData.data);
 
   cv.cvtColor(src, hsv, cv.COLOR_RGBA2RGB);
   cv.cvtColor(hsv, hsv, cv.COLOR_RGB2HSV);
 
-  // Красный цвет (пример)
-  let low = new cv.Mat(hsv.rows, hsv.cols, hsv.type(), [0, 120, 70, 0]);
-  let high = new cv.Mat(hsv.rows, hsv.cols, hsv.type(), [10, 255, 255, 255]);
+  let low = new cv.Mat(hsv.rows, hsv.cols, hsv.type(), [0,120,70,0]);
+  let high = new cv.Mat(hsv.rows, hsv.cols, hsv.type(), [10,255,255,255]);
 
   cv.inRange(hsv, low, high, mask);
 
   let contours = new cv.MatVector();
   let hierarchy = new cv.Mat();
+
   cv.findContours(mask, contours, hierarchy, cv.RETR_EXTERNAL, cv.CHAIN_APPROX_SIMPLE);
 
   for (let i = 0; i < contours.size(); i++) {
-    let cnt = contours.get(i);
-    let rect = cv.boundingRect(cnt);
-    ctx.strokeStyle = "lime";
-    ctx.lineWidth = 3;
-    ctx.strokeRect(rect.x, rect.y, rect.width, rect.height);
+    let rect = cv.boundingRect(contours.get(i));
+    cv.rectangle(src,
+      new cv.Point(rect.x, rect.y),
+      new cv.Point(rect.x + rect.width, rect.y + rect.height),
+      [0,255,0,255],
+      2
+    );
   }
+
+  cv.imshow(canvas, src);
 
   src.delete();
   hsv.delete();
@@ -57,7 +65,3 @@ function processVideo() {
 
   requestAnimationFrame(processVideo);
 }
-
-setTimeout(() => {
-  processVideo();
-}, 2000);
